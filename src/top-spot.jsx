57d4cedaaee2e39b6d5ddd666106caf4b456783c
jayluvsdivn <jayluvsdivn@gmail.com>
2019-05-16 07:38:37 -0700

    changes formatting in a couple places, passes tests

--- src/top-spot.jsx
+++ src/top-spot.jsx
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-
-export default props => (
-  <div className='well'>
-    <h4>{props.name}</h4>
-    <p>{props.description}</p>
-    <a href={`https://maps.google.com/?q=${props.location[0]},${props.location[1]}`}><button className="btn btn-primary">Link</button></a>
-  </div>
-);
+import React, { component } from React;
+export default (props) => {
+  return(
+    <div className='well'>
+      <h4>{props.name}</h4>
+      <p>{props.description}</p>
+      <a href={`https://maps.google.com/?q=${props.location[0]},${props.location[1]}`}><button className="btn btn-primary">Link</button></a>
+    </div>
+  );
+}
